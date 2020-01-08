3679f40a3adedc40da04d94b659c073d2c041304
Michael Bonner <michaelwbonner@gmail.com>
2020-01-08 14:23:53 -0700

    pin placement tool

---
+++ components/map-simple.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const MapSimple = ({ children }) => {
+  return (
+    <div>
+      <div className="fixed z-10 w-full">{children}</div>
+      <img
+        alt="map background"
+        src="/pch-background.svg"
+        style={{ maxWidth: "none", width: "3200px", height: "2400px" }}
+      />
+    </div>
+  );
+};
+
+export default MapSimple;
---
+++ pages/place/pin.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from "react";
+import Head from "next/head";
+import MapSimple from "../../components/map-simple";
+
+const Pin = () => {
+  useEffect(() => {
+    // add when mounted
+    document.addEventListener("mousedown", handleClick);
+    // return function to be called when unmounted
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    };
+  }, []);
+
+  const handleClick = e => {
+    alert(`x: ${e.pageX} | y: ${e.pageY} `);
+    console.log(e);
+    // if (node.current.contains(e.target)) {
+    //   // inside click
+    //   return;
+    // }
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>Primary Children's Hospital Patient Stories</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          property="og:url"
+          content="https://primary-childrens-stories.michaelbonner.now.sh/"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Primary Children's Hospital Stories"
+        />
+        <meta
+          property="og:description"
+          content="A map showcasing some of Primary Children's Hospital's success stories"
+        />
+        <meta
+          property="og:image"
+          content="https://primary-childrens-stories.michaelbonner.now.sh/og-image.jpg"
+        />
+      </Head>
+
+      <MapSimple />
+    </div>
+  );
+};
+
+export default Pin;
