78a5459803b7bd7dc388bc4c725b78e11dbd2b15
Michael Bonner <michaelwbonner@gmail.com>
2020-01-19 13:30:43 -0700

    update nav

--- components/nav.js
+++ components/nav.js
@@ -4,6 +4,7 @@ import Twitter from "../components/twitter";
 import { FacebookShareButton, TwitterShareButton } from "react-share";
 import useContentfulContent from "../shared/hooks/useContentfulContent";
 import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+import YoutubeEmbed from "./youtube-embed";
 
 const Nav = () => {
   const [activeTab, setActiveTab] = useState("");
@@ -21,128 +22,103 @@ const Nav = () => {
     });
   }, []);
 
+  const NavButton = ({ link, text }) => {
+    return (
+      <button
+        className={`${
+          activeTab === link ? "text-blue-800" : "text-blue-600"
+        } w-1/3 py-3 px-8 text-base font-bold focus:outline-none h-full`}
+        onClick={() => {
+          if (activeTab === link) {
+            setActiveTab("");
+          } else {
+            setActiveTab(link);
+          }
+        }}
+      >
+        <span className="inline-block mt-1">{text}</span>
+      </button>
+    );
+  };
+
+  const TabContentWrapper = ({ children, tabLink }) => {
+    if (activeTab !== tabLink) {
+      return "";
+    }
+
+    return (
+      <div
+        className={`w-full md:w-1/2 xl:w-1/3 mt-2 mx-auto py-12 px-12 text-center text-gray-600 rounded-lg bg-gray-100 shadow-md`}
+      >
+        {children}
+      </div>
+    );
+  };
+
   return (
     <div>
-      <div className="fixed right-0 top-0 flex flex-col justify-end mr-4 md:mr-8 w-24 text-center z-50">
+      <div className="fixed right-0 top-0 flex flex-col justify-end mr-4 md:mr-8 w-20 text-center z-50">
         <button
-          className="inline-block py-3 md:py-6 px-2 md:px-4 bg-gray-200 text-gray-600 text-sm uppercase rounded-b-lg shadow-md z-30 focus:outline-none"
+          className="inline-block py-3 md:pt-8 md:pb-2 px-2 bg-gray-200 text-gray-600 text-sm rounded-b-lg shadow-md z-30 focus:outline-none font-bold"
           onClick={() => {
             setShareOpen(!shareOpen);
           }}
         >
           Share
         </button>
-        <FacebookShareButton
-          className={`${
-            shareOpen ? "flex" : "hidden"
-          } items-center justify-center -mt-1 pt-3 pb-2 px-4 bg-facebook-500 hover:bg-facebook-600 text-white text-sm uppercase rounded-b-lg shadow-md z-20 focus:outline-none`}
-          url={
-            process.browser
-              ? window.location.href
-              : "https://primary-childrens-stories.michaelbonner.now.sh/"
-          }
-        >
-          <Facebook className="w-8 fill-current" />
-        </FacebookShareButton>
-        <TwitterShareButton
-          className={`${
-            shareOpen ? "flex" : "hidden"
-          } items-center justify-center -mt-1 pt-3 pb-2 px-4 bg-twitter-500 hover:bg-twitter-600 text-white text-sm uppercase rounded-b-lg shadow-md z-10 focus:outline-none`}
-          url={
-            process.browser
-              ? window.location.href
-              : "https://primary-childrens-stories.michaelbonner.now.sh/"
-          }
-        >
-          <Twitter className="w-8 fill-current" />
-        </TwitterShareButton>
+        {shareOpen && (
+          <FacebookShareButton
+            className={`flex items-center justify-center -mt-1 pt-3 pb-2 px-4 bg-facebook-500 hover:bg-facebook-600 text-white text-sm rounded-b-lg shadow-md z-20 focus:outline-none`}
+            url={
+              process.browser
+                ? window.location.href
+                : "https://primary-childrens-stories.michaelbonner.now.sh/"
+            }
+          >
+            <Facebook className="w-8 fill-current" />
+          </FacebookShareButton>
+        )}
+        {shareOpen && (
+          <TwitterShareButton
+            className={`flex items-center justify-center -mt-1 pt-3 pb-2 px-4 bg-twitter-500 hover:bg-twitter-600 text-white text-sm rounded-b-lg shadow-md z-10 focus:outline-none`}
+            url={
+              process.browser
+                ? window.location.href
+                : "https://primary-childrens-stories.michaelbonner.now.sh/"
+            }
+          >
+            <Twitter className="w-8 fill-current" />
+          </TwitterShareButton>
+        )}
       </div>
       <div className="px-4">
-        <nav className="relative mt-16 flex w-full md:w-1/3 mx-auto text-gray-600 text-center rounded-lg bg-gray-200 shadow-md z-30">
-          <button
-            className={`${
-              activeTab === "about" ? "text-gray-900" : "text-gray-600"
-            } w-1/3 py-3 px-8 text-sm font-medium uppercase focus:outline-none`}
-            onClick={() => {
-              if (activeTab === "about") {
-                setActiveTab("");
-              } else {
-                setActiveTab("about");
-              }
-            }}
-          >
-            <svg
-              viewBox="0 0 100 100"
-              xmlns="http://www.w3.org/2000/svg"
-              className="fill-current w-4 h-4 mx-auto"
-            >
-              <circle cx="50" cy="50" r="50" />
-            </svg>
-            <span className="inline-block mt-1">About</span>
-          </button>
-          <button
-            className={`${
-              activeTab === "search" ? "text-gray-900" : "text-gray-600"
-            } w-1/3 py-3 px-8 text-sm font-medium uppercase focus:outline-none`}
-            onClick={() => {
-              if (activeTab === "search") {
-                setActiveTab("");
-              } else {
-                setActiveTab("search");
-              }
-            }}
-          >
-            <svg
-              viewBox="0 0 100 100"
-              xmlns="http://www.w3.org/2000/svg"
-              className="fill-current w-4 h-4 mx-auto"
-            >
-              <circle cx="50" cy="50" r="50" />
-            </svg>
-            <span className="inline-block mt-1">Search</span>
-          </button>
-          <button
-            className={`${
-              activeTab === "submit" ? "text-gray-900" : "text-gray-600"
-            } w-1/3 py-3 px-8 text-sm font-medium uppercase focus:outline-none`}
-            onClick={() => {
-              if (activeTab === "submit") {
-                setActiveTab("");
-              } else {
-                setActiveTab("submit");
-              }
-            }}
-          >
-            <svg
-              viewBox="0 0 100 100"
-              xmlns="http://www.w3.org/2000/svg"
-              className="fill-current w-4 h-4 mx-auto"
-            >
-              <circle cx="50" cy="50" r="50" />
-            </svg>
-            <span className="inline-block mt-1">Submit</span>
-          </button>
+        <nav className="relative mt-16 flex w-full md:w-1/2 xl:w-1/3 mx-auto text-blue-600 text-center z-30">
+          <div className="rounded-lg bg-gray-200 shadow-md px-4 py-1">
+            <img
+              src="/images/primary-childrens-hospital-logo.svg"
+              alt="Primary Children's Hospital"
+              style={{ width: "180px", height: "70px" }}
+            />
+          </div>
+          <div className="rounded-lg bg-gray-200 shadow-md ml-2 items-center flex-1">
+            <NavButton text="About" link="about" />
+            <NavButton text="Search" link="search" />
+            <NavButton text="Submit" link="submit" />
+          </div>
         </nav>
-        <div
-          className={`${
-            activeTab === "about"
-              ? "w-full md:w-1/3 mx-auto text-gray-600 rounded-lg bg-gray-100 p-4"
-              : "hidden"
-          }`}
-          dangerouslySetInnerHTML={{
-            __html: documentToHtmlString(aboutTabContent)
-          }}
-        ></div>
-        <div
-          className={`${
-            activeTab === "search"
-              ? "w-full md:w-1/3 mx-auto text-gray-600 rounded-lg bg-gray-100 p-4"
-              : "hidden"
-          }`}
-        >
+        <TabContentWrapper tabLink="about">
+          <YoutubeEmbed youtubeId={"Q-lhnGPj_tk"} />
+          <div
+            className="mt-4"
+            dangerouslySetInnerHTML={{
+              __html: documentToHtmlString(aboutTabContent)
+            }}
+          ></div>
+        </TabContentWrapper>
+        <TabContentWrapper tabLink="search">
           {contentfulContent.categories.map(category => (
             <button
-              className={`w-1/3 py-3 px-8 text-sm font-medium uppercase focus:outline-none`}
+              className={`w-1/3 py-3 px-8 text-sm font-medium focus:outline-none`}
               key={category.sys.id}
               onClick={() => setActiveTab("search")}
             >
@@ -157,14 +133,8 @@ const Nav = () => {
               <span className="inline-block mt-1">{category.fields.name}</span>
             </button>
           ))}
-        </div>
-        <div
-          className={`${
-            activeTab === "submit"
-              ? "w-full md:w-1/3 mx-auto text-gray-600 rounded-lg bg-gray-100 p-4"
-              : "hidden"
-          }`}
-        >
+        </TabContentWrapper>
+        <TabContentWrapper tabLink="submit">
           <div
             dangerouslySetInnerHTML={{
               __html: documentToHtmlString(submitTabContent)
@@ -172,11 +142,11 @@ const Nav = () => {
           />
           <a
             href="https://intermountainhealthcare.org/"
-            className="inline-block bg-blue-400 text-blue-100 mt-2 py-2 px-4 rounded uppercase"
+            className="inline-block mt-6 bg-blue-400 text-blue-100 mt-2 py-3 px-12 rounded"
           >
-            Submit Your Story
+            Submit
           </a>
-        </div>
+        </TabContentWrapper>
       </div>
     </div>
   );
