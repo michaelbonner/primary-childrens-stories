17ab9c664f7bae2573af7e67fcade388e47625ba
Michael Bonner <michaelwbonner@gmail.com>
2020-01-23 12:43:07 -0700

    add gtm

--- pages/index.js
+++ pages/index.js
@@ -10,28 +10,6 @@ const Home = () => {
     <div>
       <Head>
         <title>Primary Children's Hospital Patient Stories</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          property="og:url"
-          content="https://primary-childrens-stories.michaelbonner.now.sh/"
-        />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:title"
-          content="Primary Children's Hospital Stories"
-        />
-        <meta
-          property="og:description"
-          content="A map showcasing some of Primary Children's Hospital's success stories"
-        />
-        <meta
-          property="og:image"
-          content="https://primary-childrens-stories.michaelbonner.now.sh/og-image.jpg"
-        />
-        <link
-          href="https://fonts.googleapis.com/css?family=Public+Sans:400,400i,500,500i,600,600i,700,700i&display=swap"
-          rel="stylesheet"
-        />
       </Head>
 
       <Map>
