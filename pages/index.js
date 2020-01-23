4f88940d08166ce2eb3ca9c1f927d63002f32b4d
Michael Bonner <michaelwbonner@gmail.com>
2020-01-23 10:01:13 -0700

    update fonts

--- pages/index.js
+++ pages/index.js
@@ -28,6 +28,10 @@ const Home = () => {
           property="og:image"
           content="https://primary-childrens-stories.michaelbonner.now.sh/og-image.jpg"
         />
+        <link
+          href="https://fonts.googleapis.com/css?family=Public+Sans:400,400i,500,500i,600,600i,700,700i&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <Map>
