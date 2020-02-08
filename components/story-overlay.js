a867a25ba080c35539e68bcf889b1ce3d6913a6d
Michael Bonner <michaelwbonner@gmail.com>
2020-02-08 11:59:33 -0700

    remove hidden elements from dom

--- components/story-overlay.js
+++ components/story-overlay.js
@@ -135,113 +135,109 @@ const StoryOverlay = ({
   };
 
   return (
-    <>
-      <div
-        className={
-          activeStory ? "fixed z-20 inset-0 bg-white opacity-50" : "hidden"
-        }
-        onClick={() => {
-          setShareOpen(false);
-          setActiveStory(null);
-        }}
-      />
-      <div
-        className={
-          activeStory ? "fixed z-30 w-full py-8 md:py-16 inset-y-0" : "hidden"
-        }
-      >
+    activeStory && (
+      <>
         <div
-          className="flex h-full md:pb-32 px-3"
-          onClick={e => {
+          className={`fixed z-20 inset-0 bg-white opacity-50`}
+          onClick={() => {
             setShareOpen(false);
             setActiveStory(null);
           }}
-        >
+        />
+        <div className={"fixed z-30 w-full py-8 md:py-16 inset-y-0"}>
           <div
-            className="relative bg-white w-full md:w-2/3 xl:w-1/2 mx-auto py-8 md:py-8 px-8 md:px-16 rounded-lg shadow-md"
+            className="flex h-full md:pb-32 px-3"
             onClick={e => {
-              e.stopPropagation();
+              setShareOpen(false);
+              setActiveStory(null);
             }}
           >
-            <div className="absolute left:0 lg:right-0 top-0 -ml-6 lg:ml-0 mt-2 lg:mt-1 lg:-mr-12">
-              <button
-                className="lg:-mr-32 bg-white px-6 py-4 shadow-lg rounded-lg font-bold text-gray-600"
-                onClick={() => {
-                  setShareOpen(false);
-                  setActiveStory(null);
-                }}
-              >
-                X
-              </button>
-            </div>
-            {activeStory && (
-              <>
-                <div className="absolute right-0 top-0 flex flex-col justify-end mr-8 md:mr-16 w-24 text-center">
-                  <button
-                    className="inline-block py-3 md:pt-8 md:pb-3 px-2 md:px-4 bg-blue-500 text-blue-100 text-sm font-bold rounded-b-lg shadow-md z-50 focus:outline-none"
-                    onClick={() => {
-                      setShareOpen(!shareOpen);
-                    }}
-                  >
-                    Share
-                  </button>
-                  <FacebookShareButton
-                    className={`${
-                      shareOpen ? "flex" : "hidden"
-                    } items-center justify-center -mt-1 pt-3 pb-2 px-4 bg-facebook-500 hover:bg-facebook-600 text-white text-sm uppercase rounded-b-lg shadow-md z-40 focus:outline-none`}
-                    url={url}
-                    resetButtonStyle={false}
-                  >
-                    <Facebook className="w-8 fill-current" />
-                  </FacebookShareButton>
-                  <TwitterShareButton
-                    className={`${
-                      shareOpen ? "flex" : "hidden"
-                    } items-center justify-center -mt-1 pt-3 pb-2 px-4 bg-twitter-500 hover:bg-twitter-600 text-white text-sm uppercase rounded-b-lg shadow-md z-30 focus:outline-none`}
-                    url={url}
-                    resetButtonStyle={false}
-                  >
-                    <Twitter className="w-8 fill-current" />
-                  </TwitterShareButton>
-                  <button
-                    className={`${
-                      shareOpen ? "flex" : "hidden"
-                    } flex items-center justify-center -mt-1 pt-3 pb-2 px-4 bg-gray-400 hover:bg-gray-500 text-gray-600 text-sm uppercase rounded-b-lg shadow-md z-20 focus:outline-none`}
-                    onClick={() => window.open(url)}
-                  >
-                    <LinkIcon className="w-8 p-1 fill-current" />
-                  </button>
-                </div>
-                <div className="h-full pt-12 flex flex-col">
-                  <div className="story-content overflow-y-scroll flex-1">
-                    <div className="text-xl font-bold leading-relaxed mb-6">
-                      {title}
+            <div
+              className="relative bg-white w-full md:w-2/3 xl:w-1/2 mx-auto py-8 md:py-8 px-8 md:px-16 rounded-lg shadow-md"
+              onClick={e => {
+                e.stopPropagation();
+              }}
+            >
+              <div className="absolute left:0 lg:right-0 top-0 -ml-6 lg:ml-0 mt-2 lg:mt-1 lg:-mr-12">
+                <button
+                  className="lg:-mr-32 bg-white px-6 py-4 shadow-lg rounded-lg font-bold text-gray-600"
+                  onClick={() => {
+                    setShareOpen(false);
+                    setActiveStory(null);
+                  }}
+                >
+                  X
+                </button>
+              </div>
+              {activeStory && (
+                <>
+                  <div className="absolute right-0 top-0 flex flex-col justify-end mr-8 md:mr-16 w-24 text-center">
+                    <button
+                      className="inline-block py-3 md:pt-8 md:pb-3 px-2 md:px-4 bg-blue-500 text-blue-100 text-sm font-bold rounded-b-lg shadow-md z-50 focus:outline-none"
+                      onClick={() => {
+                        setShareOpen(!shareOpen);
+                      }}
+                    >
+                      Share
+                    </button>
+                    <FacebookShareButton
+                      className={`${
+                        shareOpen ? "flex" : "hidden"
+                      } items-center justify-center -mt-1 pt-3 pb-2 px-4 bg-facebook-500 hover:bg-facebook-600 text-white text-sm uppercase rounded-b-lg shadow-md z-40 focus:outline-none`}
+                      url={url}
+                      resetButtonStyle={false}
+                    >
+                      <Facebook className="w-8 fill-current" />
+                    </FacebookShareButton>
+                    <TwitterShareButton
+                      className={`${
+                        shareOpen ? "flex" : "hidden"
+                      } items-center justify-center -mt-1 pt-3 pb-2 px-4 bg-twitter-500 hover:bg-twitter-600 text-white text-sm uppercase rounded-b-lg shadow-md z-30 focus:outline-none`}
+                      url={url}
+                      resetButtonStyle={false}
+                    >
+                      <Twitter className="w-8 fill-current" />
+                    </TwitterShareButton>
+                    <button
+                      className={`${
+                        shareOpen ? "flex" : "hidden"
+                      } flex items-center justify-center -mt-1 pt-3 pb-2 px-4 bg-gray-400 hover:bg-gray-500 text-gray-600 text-sm uppercase rounded-b-lg shadow-md z-20 focus:outline-none`}
+                      onClick={() => window.open(url)}
+                    >
+                      <LinkIcon className="w-8 p-1 fill-current" />
+                    </button>
+                  </div>
+                  <div className="h-full pt-12 flex flex-col">
+                    <div className="story-content overflow-y-scroll flex-1">
+                      <div className="text-xl font-bold leading-relaxed mb-6">
+                        {title}
+                      </div>
+                      {media.map(item => {
+                        return printMedia(item);
+                      })}
+                      <div
+                        className="text-base leading-relaxed mt-4"
+                        dangerouslySetInnerHTML={{
+                          __html: body
+                        }}
+                      />
                     </div>
-                    {media.map(item => {
-                      return printMedia(item);
-                    })}
-                    <div
-                      className="text-base leading-relaxed mt-4"
-                      dangerouslySetInnerHTML={{
-                        __html: body
-                      }}
-                    />
+                    <div className="mt-6">
+                      <div
+                        className="story-content"
+                        dangerouslySetInnerHTML={{
+                          __html: footerText
+                        }}
+                      />
+                    </div>
                   </div>
-                  <div className="mt-6">
-                    <div
-                      className="story-content"
-                      dangerouslySetInnerHTML={{
-                        __html: footerText
-                      }}
-                    />
-                  </div>
-                </div>
-              </>
-            )}
+                </>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </>
+      </>
+    )
   );
 };
 export default StoryOverlay;
