64941a1407802ee57d4267fee8022d41a8732935
Michael Bonner <michaelwbonner@gmail.com>
2020-02-12 11:16:40 -0700

    clean up youtube embed

--- shared/youtubeEmbed.js
+++ shared/youtubeEmbed.js
@@ -1,27 +1,32 @@
 import YouTube from "react-youtube";
 
-const youtubePlayer = videoId => {
-  return (
-    <YouTube
-      videoId={videoId}
-      id={videoId}
-      className={`w-full my-4`}
-      containerClassName={`w-full`}
-    />
-  );
-};
-
 const youtubeEmbed = youtubeLink => {
-  const found = youtubeLink.match(/[\?\&]v\=([a-zA-Z0-9\_\-]+)/i);
-  if (found && found.length > 1) {
-    return youtubePlayer(found[1]);
+  const found = youtubeLink.match(
+    /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/i
+  );
+  if (found && found.length > 2) {
+    return (
+      <YouTube
+        videoId={found[2]}
+        id={found[2]}
+        className={`w-full my-4`}
+        containerClassName={`w-full`}
+        opts={{
+          playerVars: {
+            rel: 0,
+            showinfo: 0,
+            modestbranding: 1,
+            listType: "user_uploads"
+          }
+        }}
+      />
+    );
   }
 
-  const found2 = youtubeLink.match(/youtu\.be\/([a-zA-Z0-9\_\-]+)/i);
-  if (found2 && found2.length > 1) {
-    return youtubePlayer(found2[1]);
-  }
-
-  return <p className="my-4 text-gray-400 italic">Invalid YouTube link</p>;
+  return (
+    <p className="my-4 text-gray-400 italic" data-url={youtubeLink}>
+      Invalid YouTube link
+    </p>
+  );
 };
 export default youtubeEmbed;
