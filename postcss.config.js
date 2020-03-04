38f4bf1b78149d2d59f56ef3b30190b93419ef4b
Michael Bonner <michaelwbonner@gmail.com>
2020-03-03 18:19:07 -0700

    add toastify classes to whitelist

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
   // Specify the paths to all of the template files in your project
   content: ["./pages/**/*.js", "./components/**/*.js"],
   whitelist: ["html", "body"],
+  whitelistPatterns: [/^Toastify/],
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
