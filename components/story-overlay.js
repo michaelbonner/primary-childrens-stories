01b4579f8d27473953fce15d5df93166dae75789
Michael Bonner <michaelwbonner@gmail.com>
2020-05-07 09:47:31 -0600

    style adjustments to next and previous story buttons

--- components/story-overlay.js
+++ components/story-overlay.js
@@ -49,7 +49,6 @@ const StoryOverlay = ({
       sec = 0;
     } else {
       sec = sec + 1;
-      console.log(sec);
     }
 
     setActiveStory(index[sec]);
@@ -62,7 +61,6 @@ const StoryOverlay = ({
       sec = 0;
     } else {
       sec = sec + 1;
-      console.log(sec);
     }
     setActiveStory(index[sec - 1]);
   };
@@ -235,7 +233,7 @@ const StoryOverlay = ({
                     </button>
                   </div>
                   <div className="h-full pt-12 flex flex-col">
-                    <div className="story-content overflow-y-scroll flex-1">
+                    <div className="overflow-y-scroll flex-1">
                       <div className="text-xl font-bold leading-relaxed mb-6">
                         {title}
                       </div>
@@ -252,35 +250,19 @@ const StoryOverlay = ({
                         </div>
                       )}
                       <div
-                        className="text-base leading-relaxed mt-4"
+                        className="story-content text-base leading-relaxed mt-4"
                         dangerouslySetInnerHTML={{
                           __html: body,
                         }}
                       />
-                    </div>
-                    <div className="w-full flex mt-4">
-                      <div onClick={prevStory} className="inline-block  w-1/3">
-                        <img
-                          className="h-8"
-                          alt="back arrow"
-                          src="/images/arrow-back-outline.svg"
-                        />
-                      </div>
-                      <div className="inline-block w-1/3 text-center">
+                      <div className="flex justify-center text-center mb-8">
                         <a
                           href="https://intermountainhealthcare.org/locations/primary-childrens-hospital/here-kids-win-stories/"
                           target="_blank"
-                          className="inline-block bg-blue-400 text-blue-100 py-2 px-4 rounded"
+                          className="inline-block mt-6 bg-blue-500 text-blue-100 mt-2 py-3 px-12 rounded"
                         >
                           Share Your Story
                         </a>
-                      </div>
-                      <div onClick={nextStory} className="inline-block  w-1/3">
-                        <img
-                          className="h-8 float-right"
-                          alt="next arrow"
-                          src="/images/arrow-forward-outline.svg"
-                        />
                       </div>
                     </div>
                     <div>
@@ -290,6 +272,80 @@ const StoryOverlay = ({
                           __html: footerText,
                         }}
                       />
+                    </div>
+                    <div className="w-full flex justify-between items-center mt-4 text-blue-400 text-sm">
+                      <button
+                        onClick={prevStory}
+                        className="inline-block flex items-center stroke-current justify-start focus:outline-none focus:text-blue-900"
+                      >
+                        <svg
+                          className="w-5 h-5"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="512"
+                          height="512"
+                          viewBox="0 0 512 512"
+                        >
+                          <title>Previous Story</title>
+                          <polyline
+                            points="244 400 100 256 244 112"
+                            style={{
+                              fill: "none",
+                              strokeLinecap: "round",
+                              strokeLinejoin: "round",
+                              strokeWidth: "48px",
+                            }}
+                          />
+                          <line
+                            x1="120"
+                            y1="256"
+                            x2="412"
+                            y2="256"
+                            style={{
+                              fill: "none",
+                              strokeLinecap: "round",
+                              strokeLinejoin: "round",
+                              strokeWidth: "48px",
+                            }}
+                          />
+                        </svg>
+                        <span>Previous Story</span>
+                      </button>
+                      <button
+                        onClick={nextStory}
+                        className="inline-block flex items-center stroke-current justify-end focus:outline-none focus:text-blue-900"
+                      >
+                        <span>Next Story</span>
+                        <svg
+                          className="w-5 h-5"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="512"
+                          height="512"
+                          viewBox="0 0 512 512"
+                        >
+                          <title>Next Story</title>
+                          <polyline
+                            points="268 112 412 256 268 400"
+                            style={{
+                              fill: "none",
+                              strokeLinecap: "round",
+                              strokeLinejoin: "round",
+                              strokeWidth: "48px",
+                            }}
+                          />
+                          <line
+                            x1="392"
+                            y1="256"
+                            x2="100"
+                            y2="256"
+                            style={{
+                              fill: "none",
+                              strokeLinecap: "round",
+                              strokeLinejoin: "round",
+                              strokeWidth: "48px",
+                            }}
+                          />
+                        </svg>
+                      </button>
                     </div>
                   </div>
                 </>
